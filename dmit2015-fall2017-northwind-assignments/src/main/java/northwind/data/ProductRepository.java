f88d2272857db919dd33d30630861e34af36fa8f
Holly Conant <hconant1@nait.ca>
2017-10-19 16:13:14 -0600

    Finished Top Ten Expensive Products

--- dmit2015-fall2017-northwind-assignments/src/main/java/northwind/data/ProductRepository.java
+++ dmit2015-fall2017-northwind-assignments/src/main/java/northwind/data/ProductRepository.java
@@ -3,6 +3,7 @@ package northwind.data;
 import java.util.List;
 
 import northwind.model.Product;
+import northwind.report.ExpensiveProductReport;
 
 public class ProductRepository extends AbstractJpaRepository<Product> {
 	private static final long serialVersionUID = 1L;
@@ -24,6 +25,15 @@ public class ProductRepository extends AbstractJpaRepository<Product> {
 				"SELECT p FROM Product p WHERE p.supplier.supplierID = :idValue", Product.class)
 				.setParameter("idValue", supplierId)
 				.getResultList();
+	}
+	
+	public List<ExpensiveProductReport> findTenMostExpensive() {
+		return getEntityManager().createQuery(
+				"SELECT new northwind.report.ExpensiveProductReport(p.productName, p.unitPrice)"
+				+ "FROM Product p "
+				+ "ORDER BY p.unitPrice DESC", 
+				ExpensiveProductReport.class).setMaxResults(10) .getResultList();
+				
 	}
 
 }
